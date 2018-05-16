2d503590f3b7f1ee950b12e7e1b73accc9b8e864
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-16 22:44:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ if katz_deli == []
   elsif 
   katz_line = ["The line is currently: "]
   katz_deli.each.with_index(1) { |name, index| 
-  katz_line << "#{index}. #{name}""}
+  katz_line << "#{index}. #{name}"}
   puts katz_line
 
 
