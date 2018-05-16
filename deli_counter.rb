3ede56733e7241e64a49fd0b5537a56410793bf4
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-16 23:10:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,6 +17,6 @@ end
 end
 
 def take_a_number(katz_deli, name)
-  place = katz_deli.each.with_index(1) 
+  place = katz_deli.index
 puts "Welcome, #{name}. You are number #{place} in line."   
 end
