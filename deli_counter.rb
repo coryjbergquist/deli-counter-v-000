15ca25640d35357831a458fab583e4cc63ddc0f2
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-16 22:29:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,10 @@
 # Write your code here.
-
+  katz_deli = []
 
 
 def line(katz_deli)
-  katz_deli = []
 if katz_deli == 0
-  puts "The line is currently empty."
+  puts "The line is currently empty.""
 elsif katz_deli != 0 
   puts katz_deli
 
