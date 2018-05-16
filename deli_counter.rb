f2f8fe67c47e249ebef737926b4b2a4c30e011b9
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-16 23:23:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,8 +17,6 @@ end
 end
 
 def take_a_number(katz_deli, name)
- katz_line = katz_deli.count
- katz_line = katz_line.to_i
- katz_line = katz_line + 1
+ katz_line = 1
 puts "Welcome, #{name}. You are number #{katz_line} in line."   
 end
