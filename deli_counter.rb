bdbf4b47578e93f6216f45fed836668b032e4c59
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-16 22:26:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,11 @@
 
 katz_deli = []
 
-def line(place)
-if place > 0 
-  puts place
-else
+def line(katz_deli)
+if katz_deli == 0
   puts "The line is currently empty."
+if katz_deli > 0 
+  puts katz_deli
+
 end
 end
