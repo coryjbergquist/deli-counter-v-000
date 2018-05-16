b0ded66ef1e636a1dd1884c7784d4a9f664b848f
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-16 22:46:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,9 +6,8 @@ def line(katz_deli)
 if katz_deli == []
   puts "The line is currently empty."
   elsif 
-  katz_line = ["The line is currently: "]
-  katz_deli.each.with_index(1) { |name, index| 
-  katz_line << #{index} #{name}}
+  katz_line = katz_deli.each.
+  
   puts katz_line
 
 
