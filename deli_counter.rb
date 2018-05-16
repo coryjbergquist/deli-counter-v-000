1b9780dd1e17778825293fe0f98b13bd3eec9334
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-16 22:39:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,8 @@ if katz_deli == []
   puts "The line is currently empty."
   elsif 
   katz_line = []
-  katz_deli.each.with_index(1) { |x| 
+  katz_deli.each.with_index(1) { |name, index| 
+  katz_line << 
   puts katz_deli
 
 
